475a9024199d3ff135abb0790a7ff58322f9e254
Ahamd24 <162799956+Ahamd24@users.noreply.github.com>
2024-03-12 09:51:21 +0430

    Delete js directory

--- js/js.js
+++
@@ -1,6 +0,0 @@
-const btnNavEl = document.querySelector(".header-buttons");
-const headerEl = document.querySelector(".header");
-
-btnNavEl.addEventListener("click", function () {
-  headerEl.classList.toggle("nav-open");
-});
